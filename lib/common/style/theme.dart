9b8a88da11ac543dad29b887438c30f342cb791b
Jackiu <jackiu1997@outlook.com>
2023-01-30 22:45:26 +0800

    Feat: use pingfang for windows

--- lib/common/style/theme.dart
+++ lib/common/style/theme.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 
 class MyTheme {
@@ -5,22 +7,45 @@ class MyTheme {
 
   MyTheme(this.primaryColor);
 
-  get lightThemeData => ThemeData.from(
+  get lightThemeData {
+    if (Platform.isWindows) {
+      return ThemeData(
         useMaterial3: true,
-        colorScheme: ColorScheme.fromSeed(
-          seedColor: primaryColor,
-          primary: primaryColor,
-          brightness: Brightness.light,
-        ),
+        colorSchemeSeed: primaryColor,
+        brightness: Brightness.light,
+        fontFamily: 'PingFang',
       );
+    }
 
-  get darkThemeData => ThemeData.from(
+    return ThemeData.from(
+      useMaterial3: true,
+      colorScheme: ColorScheme.fromSeed(
+        seedColor: primaryColor,
+        primary: primaryColor,
+        brightness: Brightness.light,
+      ),
+    );
+  }
+
+  get darkThemeData {
+    if (Platform.isWindows) {
+      return ThemeData(
         useMaterial3: true,
-        colorScheme: ColorScheme.fromSeed(
-          error: const Color.fromARGB(255, 255, 99, 71),
-          seedColor: primaryColor,
-          brightness: Brightness.dark,
-          primary: primaryColor,
-        ),
+        colorSchemeSeed: primaryColor,
+        brightness: Brightness.dark,
+        errorColor: const Color.fromARGB(255, 255, 99, 71),
+        fontFamily: 'PingFang',
       );
+    }
+
+    return ThemeData.from(
+      useMaterial3: true,
+      colorScheme: ColorScheme.fromSeed(
+        error: const Color.fromARGB(255, 255, 99, 71),
+        seedColor: primaryColor,
+        brightness: Brightness.dark,
+        primary: primaryColor,
+      ),
+    );
+  }
 }
