4c1c5710b142f429c26e814628bd57b2d459a26f
Jackiu <jackiu1997@outlook.com>
2023-02-24 20:19:55 +0800

    Feat: add areainfo fromto json

--- lib/common/models/livearea.dart
+++ lib/common/models/livearea.dart
@@ -6,4 +6,25 @@ class AreaInfo {
   String areaName = '';
   String areaPic = '';
   String shortName = '';
+
+  AreaInfo();
+
+  AreaInfo.fromJson(Map<String, dynamic> json)
+      : platform = json['platform'] ?? '',
+        areaType = json['areaType'] ?? '',
+        typeName = json['typeName'] ?? '',
+        areaId = json['areaId'] ?? '',
+        areaName = json['areaName'] ?? '',
+        areaPic = json['areaPic'] ?? '',
+        shortName = json['shortName'] ?? '';
+
+  Map<String, dynamic> toJson() => <String, dynamic>{
+        'platform': platform,
+        'areaType': areaType,
+        'typeName': typeName,
+        'areaId': areaId,
+        'areaName': areaName,
+        'areaPic': areaPic,
+        'shortName': shortName,
+      };
 }
