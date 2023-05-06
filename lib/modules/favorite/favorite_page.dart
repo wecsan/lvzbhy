171fdcbd2b5ae180c086e4710ae3dc646136937e
Jackiu <jackiu1997@outlook.com>
2023-05-06 15:04:07 +0800

    fix: fix favorite page with layoutbuilder

--- lib/modules/favorite/favorite_page.dart
+++ lib/modules/favorite/favorite_page.dart
@@ -50,19 +50,6 @@ class _RoomGridView extends GetWidget<FavoriteController> {
     required this.online,
   }) : super(key: key);
 
-  int crossAxisCount(bool dense) {
-    double screenWidth = Get.size.width;
-    int crossAxisCount = screenWidth > 1280
-        ? 4
-        : (screenWidth > 960 ? 3 : (screenWidth > 640 ? 2 : 1));
-    if (dense) {
-      crossAxisCount = screenWidth > 1280
-          ? 5
-          : (screenWidth > 960 ? 4 : (screenWidth > 640 ? 3 : 2));
-    }
-    return crossAxisCount;
-  }
-
   final refreshController = RefreshController();
 
   Future onRefresh() async {
@@ -80,31 +67,40 @@ class _RoomGridView extends GetWidget<FavoriteController> {
       final dense = Get.find<SettingsService>().enableDenseFavorites.value;
       final rooms = online ? controller.onlineRooms : controller.offlineRooms;
 
-      return SmartRefresher(
-        enablePullDown: true,
-        physics: const BouncingScrollPhysics(),
-        header: const WaterDropHeader(),
-        controller: refreshController,
-        onRefresh: onRefresh,
-        child: rooms.isNotEmpty
-            ? MasonryGridView.count(
-                padding: const EdgeInsets.all(5),
-                controller: ScrollController(),
-                crossAxisCount: crossAxisCount(dense),
-                itemCount: rooms.length,
-                itemBuilder: (context, index) =>
-                    RoomCard(room: rooms[index], dense: dense),
-              )
-            : EmptyView(
-                icon: Icons.favorite_rounded,
-                title: online
-                    ? S.of(context).empty_favorite_online_title
-                    : S.of(context).empty_favorite_offline_title,
-                subtitle: online
-                    ? S.of(context).empty_favorite_online_subtitle
-                    : S.of(context).empty_favorite_offline_subtitle,
-              ),
-      );
+      return LayoutBuilder(builder: (context, constraint) {
+        final width = constraint.maxWidth;
+        int crossAxisCount =
+            width > 1280 ? 4 : (width > 960 ? 3 : (width > 640 ? 2 : 1));
+        if (dense) {
+          crossAxisCount =
+              width > 1280 ? 5 : (width > 960 ? 4 : (width > 640 ? 3 : 2));
+        }
+        return SmartRefresher(
+          enablePullDown: true,
+          physics: const BouncingScrollPhysics(),
+          header: const WaterDropHeader(),
+          controller: refreshController,
+          onRefresh: onRefresh,
+          child: rooms.isNotEmpty
+              ? MasonryGridView.count(
+                  padding: const EdgeInsets.all(5),
+                  controller: ScrollController(),
+                  crossAxisCount: crossAxisCount,
+                  itemCount: rooms.length,
+                  itemBuilder: (context, index) =>
+                      RoomCard(room: rooms[index], dense: dense),
+                )
+              : EmptyView(
+                  icon: Icons.favorite_rounded,
+                  title: online
+                      ? S.of(context).empty_favorite_online_title
+                      : S.of(context).empty_favorite_offline_title,
+                  subtitle: online
+                      ? S.of(context).empty_favorite_online_subtitle
+                      : S.of(context).empty_favorite_offline_subtitle,
+                ),
+        );
+      });
     });
   }
 }
